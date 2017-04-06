569a18a52b06fe2e125ee21ea184638c01eb3781
Viktor <Viktorallenfalkner@gmail.com>
2017-04-06 12:38:45 -0600

    Initial Commit

---
+++ submodule-coreTests/submodule_coreTests.swift
@@ -0,0 +1,36 @@
+//
+//  submodule_coreTests.swift
+//  submodule-coreTests
+//
+//  Created by Viktor Falkner on 4/6/17.
+//  Copyright © 2017 com.proof.of.concept. All rights reserved.
+//
+
+import XCTest
+@testable import submodule_core
+
+class submodule_coreTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
